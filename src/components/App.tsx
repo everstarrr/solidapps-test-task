62164350fd0ae9fbd40af5940bd867d7a0d44cdd
everstar <72206328+everstarrr@users.noreply.github.com>
2025-02-02 23:23:59 +0300

    fixes #7

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,8 +4,38 @@ import { useLaunchParams, miniApp, useSignal, setMiniAppHeaderColor, setMiniAppB
 import { AppRoot } from '@telegram-apps/telegram-ui';
 import WebApp from '@twa-dev/sdk';
 import { useEffect, useState } from 'react';
+import { images } from './model';
 
 export function App() {
+
+  const [isImagesLoaded, setIsImagesLoaded] = useState(false);
+  const [error, setError] = useState(null); // Состояние ошибки
+
+  const preloadImages = (urls: string[]) => {
+    return Promise.all(
+      urls.map((url) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = url;
+          img.onload = () => resolve(img);
+          img.onerror = () => reject(new Error(`Не удалось загрузить изображение: ${url}`));
+        });
+      })
+    );
+  };
+
+  useEffect(() => {
+    const loadImages = async () => {
+      try {
+        await preloadImages(images);
+        setIsImagesLoaded(true);
+      } catch (err: any) {
+        setError(err);
+      }
+    };
+
+    loadImages();
+  }, []);
 
   let lp = useLaunchParams();
   const isDark = useSignal(miniApp.isDark);
@@ -13,17 +43,19 @@ export function App() {
   const [progress, setProgress] = useState(1);
 
   useEffect(() => {
-    WebApp.ready()
-    WebApp.disableVerticalSwipes()
-    WebApp.expand()
-    setMiniAppHeaderColor('#000000')
-    setMiniAppBackgroundColor("#000000")
-    const interval = setInterval(() => {
-      setProgress((prev) => (prev < 100 ? prev + 1 : 100));
-    }, 50);
+    if (isImagesLoaded) {
+      WebApp.ready()
+      WebApp.disableVerticalSwipes()
+      WebApp.expand()
+      setMiniAppHeaderColor('#000000')
+      setMiniAppBackgroundColor("#000000")
+      const interval = setInterval(() => {
+        setProgress((prev) => (prev < 100 ? prev + 1 : 100));
+      }, 50);
 
-    return () => clearInterval(interval);
-  }, []);
+      return () => clearInterval(interval);
+    }
+  }, [isImagesLoaded]);
 
   return (
     <AppRoot
---
+++ src/components/model.ts
@@ -0,0 +1,13 @@
+export const images = [
+   '../../../assets/avatar.webp',
+    '../../../assets/character.webp',
+    '../../../assets/charge_blue.webp',
+    '../../../assets/charge_red.webp',
+    '../../../assets/infoimage.webp',
+    '../../../assets/magma_planet.webp',
+    '../../../assets/magma.webp',
+    '../../../assets/navbar.webp',
+    '../../../assets/planet.webp',
+    '../../../assets/preloader.webp',
+    '../../../assets/starship.webp'
+]
